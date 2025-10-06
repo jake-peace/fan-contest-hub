b83ae9520b6985b274938885110afec1a0808f0d
Jacob Peace <jacob.peace@naimuri.com>
2025-10-06 21:29:44 +0100

    fix: issue with dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import DashboardPage from '@/components/Dashboard';
-import { AuthGetCurrentUserServer } from '@/utils/amplify-utils';
-import { AuthUser } from 'aws-amplify/auth';
 
 export default async function Page() {
-	const authUser = await AuthGetCurrentUserServer();
-
-	return <DashboardPage user={authUser as AuthUser} />;
+	return <DashboardPage />;
 }

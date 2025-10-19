db9604b049d3ebd7a92f07ad69160120f9e89a09
Jacob Peace <jacob.peace@naimuri.com>
2025-10-19 18:07:48 +0100

    feat: added list of entries to participants list

--- src/app/actions/closeVoting.ts
+++ src/app/actions/closeVoting.ts
@@ -1,7 +1,22 @@
 'use server';
 
+import { SubmissionWithScore } from '@/components/ResultsComponent';
 import { AuthGetCurrentUserServer, cookiesClient } from '@/utils/amplify-utils';
 import { cookies } from 'next/headers';
+
+const rankingPoints = new Map<number, number>([
+	[-1, 0],
+	[0, 12],
+	[1, 10],
+	[2, 8],
+	[3, 7],
+	[4, 6],
+	[5, 5],
+	[6, 4],
+	[7, 3],
+	[8, 2],
+	[9, 1],
+]);
 
 export async function closeVoting(editionId: string) {
 	try {
@@ -9,6 +24,10 @@ export async function closeVoting(editionId: string) {
 		const authUser = await AuthGetCurrentUserServer(currentRequestCookies);
 
 		const { data: edition } = await cookiesClient.models.Edition.get({ editionId: editionId });
+
+		if (!edition) {
+			throw new Error('Failed to find edition.');
+		}
 
 		const contest = await edition?.contest();
 
@@ -20,6 +39,69 @@ export async function closeVoting(editionId: string) {
 			editionId: editionId,
 			phase: 'RESULTS',
 		});
+
+		// get all non rejected submissions
+		const { data: submissionData } = await cookiesClient.models.Submission.list({
+			filter: {
+				editionId: { eq: editionId },
+				rejected: { eq: false },
+			},
+		});
+
+		if (!submissionData) {
+			throw new Error('Cannot close voting when there are no songs to vote for.');
+		}
+
+		// get all rankings
+		const rankingData = (await edition.rankings()).data;
+
+		// calculate score for all submissions
+		let submissionsWithScores: SubmissionWithScore[] = [];
+
+		submissionData.forEach((s) => {
+			const counts = new Array<number>(10).fill(0);
+
+			// Iterate over every inner array in the external data
+			for (const innerArray of rankingData.map((r) => r.rankingList as string[]) as string[][]) {
+				// Check the first 10 positions (index 0 to 9) of the current inner array
+				const limit = Math.min(innerArray.length, 10);
+
+				for (let i = 0; i < limit; i++) {
+					if (innerArray[i] === s.submissionId) {
+						// Increment the count for that specific index (i)
+						counts[i]++;
+					}
+				}
+			}
+
+			let score = 0;
+			counts.forEach((c, index) => {
+				if (c !== 0) {
+					score = score + c * (rankingPoints.get(index) as number);
+				}
+			});
+
+			submissionsWithScores = [...submissionsWithScores, { ...s, score: score }];
+		});
+
+		// order submissions by score
+		const orderedSubmissions = [...submissionsWithScores].sort((a, b) => b.score - a.score);
+
+		// ranking is index + 1
+		let submissionsWithRanks: SubmissionWithScore[] = [];
+
+		orderedSubmissions.forEach((s, index) => {
+			submissionsWithRanks = [...submissionsWithRanks, { ...s, rank: index + 1 }];
+		});
+
+		await Promise.all(
+			orderedSubmissions.map(async (s) => {
+				await cookiesClient.models.Submission.update({
+					submissionId: s.submissionId,
+					rank: s.rank,
+				});
+			})
+		);
 
 		if (errors) {
 			throw new Error(errors[0].message);
--- src/app/api/contest/[contestId]/participants/route.ts
+++ src/app/api/contest/[contestId]/participants/route.ts
@@ -1,7 +1,22 @@
+import { ProfileWithEntries } from '@/components/MembersList';
 import { cookiesClient } from '@/utils/amplify-utils';
 import { NextResponse } from 'next/server';
 
 type Params = Promise<{ contestId: string }>;
+
+const rankingPoints = new Map<number, number>([
+	[-1, 0],
+	[0, 12],
+	[1, 10],
+	[2, 8],
+	[3, 7],
+	[4, 6],
+	[5, 5],
+	[6, 4],
+	[7, 3],
+	[8, 2],
+	[9, 1],
+]);
 
 export async function GET(request: Request, segmentData: { params: Params }) {
 	try {
@@ -10,15 +25,69 @@ export async function GET(request: Request, segmentData: { params: Params }) {
 
 		const { data: contestData } = await cookiesClient.models.Contest.get({ contestId: contestId });
 
+		if (!contestData) {
+			throw new Error('Cannot find contest');
+		}
+
 		const { data } = await cookiesClient.models.Profile.list({
 			filter: {
 				or: (contestData?.participants as string[]).map((id: string) => ({ userId: { eq: id } })),
 			},
 		});
 
+		const editionsData = (await contestData.editions()).data;
+
+		if (!editionsData) {
+			throw new Error('Failed to find edition data.');
+		}
+
+		const editionList = editionsData
+			.filter((e) => e.phase !== 'SUBMISSION' && e.phase !== 'UPCOMING' && e.phase !== 'VOTING')
+			.map((e) => e.editionId);
+
+		const { data: allContestSubmissions } = await cookiesClient.models.Submission.list({
+			filter: {
+				or: (editionList as string[]).map((e: string) => ({ editionId: { eq: e } })),
+			},
+		});
+
+		const allContestSubmissionsWithScores = await Promise.all(
+			allContestSubmissions.map(async (s) => {
+				const { data: submissionData } = await cookiesClient.models.Submission.get({ submissionId: s.submissionId });
+
+				if (!submissionData) {
+					throw new Error('Cannot find submission');
+				}
+
+				const { data: rankingData } = await cookiesClient.models.Ranking.list({
+					filter: {
+						editionId: { eq: submissionData.editionId as string },
+					},
+				});
+
+				if (!rankingData) {
+					throw new Error('Failed to find edition data.');
+				}
+
+				let score = 0;
+
+				rankingData.forEach((r) => {
+					const songScore = rankingPoints.get(r.rankingList?.indexOf(s.submissionId) as number) as number;
+					score = score + songScore;
+				});
+				return { ...s, score: score };
+			})
+		);
+
+		const participantsWithEntries: ProfileWithEntries[] = data.map((p) => {
+			return { ...p, entries: allContestSubmissionsWithScores.filter((s) => s.userId === p.userId) };
+			// particpantsWithEntries = [...particpantsWithEntries, { ...p, entries: participantEntries }];
+			// console.log(particpantsWithEntries);
+		});
+
 		// 2. The client never sees the auth token or the GraphQL endpoint.
 		// The response is a plain JSON object.
-		return NextResponse.json({ participants: data });
+		return NextResponse.json({ participants: participantsWithEntries });
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	} catch (error: any) {
 		// 3. Handle authentication failures (e.g., redirect or return 401)

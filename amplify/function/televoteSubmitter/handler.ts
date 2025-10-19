06336c32fa73f77071cb4864b6d9b9a4038298c5
Jacob Peace <jacob.peace@naimuri.com>
2025-10-19 20:25:53 +0100

    chore: fix env

--- amplify/function/televoteSubmitter/handler.ts
+++ amplify/function/televoteSubmitter/handler.ts
@@ -3,11 +3,11 @@ import { v4 } from 'uuid';
 import { generateClient } from 'aws-amplify/data';
 import { Amplify } from 'aws-amplify';
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
-import { env } from '$amplify/env/televoteSubmitter';
+// import { env } from '$amplify/env/televoteSubmitter';
 
-const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
+// const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
 
-Amplify.configure(resourceConfig, libraryOptions);
+// Amplify.configure(resourceConfig, libraryOptions);
 
 // 2. Generate the client after configuring
 const client = generateClient<Schema>();
